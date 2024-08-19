4d3424b64876e9bba293ad5c3737600b11204c18
Rachit Gupta <rachitgupta1901@gmail.com>
2024-08-19 19:54:24 -0400

    Working on computer feature for game

--- App.java
+++ App.java
@@ -2,19 +2,27 @@ public class App {
 
     private static int turnOrder;
     private static String winner;
-    public static void main(String Args[]){
+
+    public static void main(String Args[]) {
         TicTacToeBoard board = TicTacToeBoard.getInstance();
-        turnOrder = 0;
-        do{
-            board.printBoard();
-            if(turnOrder % 2 == 0){
-                board.playX();
-            } else {
-                board.playO();
-            }
-            turnOrder += 1;
-            winner = board.checkWinner();
-        }while(winner == null || turnOrder < 9);
+        boolean whoToPlay = board.playAgainstComputer();
+
+        if (whoToPlay) {
+            turnOrder = 0;
+            do {
+                board.printBoard();
+                if (turnOrder % 2 == 0) {
+                    board.playX();
+                } else {
+                    board.playO();
+                }
+                turnOrder += 1;
+                winner = board.checkWinner();
+                System.out.println(winner);
+            } while (winner == null && turnOrder < 9);
+        } else {
+            
+        }
 
     }
 }
--- TicTacToeBoard.java
+++ TicTacToeBoard.java
@@ -10,9 +10,7 @@ public class TicTacToeBoard {
 
     private static TicTacToeBoard ticTacToeBoard;
 
-    
-
-    private TicTacToeBoard(){
+    private TicTacToeBoard() {
         for (int i = 0; board.length > i; i += 1) {
             for (int j = 0; board[i].length > j; j += 1) {
                 board[i][j] = '_';
@@ -21,72 +19,104 @@ public class TicTacToeBoard {
 
     }
 
-    public static TicTacToeBoard getInstance(){
-        if(ticTacToeBoard == null){
+    public static TicTacToeBoard getInstance() {
+        if (ticTacToeBoard == null) {
             ticTacToeBoard = new TicTacToeBoard();
         }
 
         return ticTacToeBoard;
     }
 
-    public void printBoard(){
+    public void printBoard() {
         for (char[] cs : board) {
-            for(int i = 0 ; cs.length > i; i += 1){
+            for (int i = 0; cs.length > i; i += 1) {
 
             }
         }
 
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board[0].length; j++) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
                 System.out.print(board[i][j]);
             }
             System.out.print("\n");
         }
     }
 
-    public void playX(){
-        boolean notValid = true;
-        int xPos;
-        int yPos;
+    public boolean playAgainstComputer() {
+        String computerOrPlayer;
         do {
-            System.out.println("What Row? 1-3");
-            xPos = clamp(input.nextInt(),1,3) - 1;
-            System.out.println("What Column? 1-3");
-            yPos = clamp(input.nextInt(),1,3) - 1;
-
-                if(board[xPos][yPos] == '_'){
-                    board[xPos][yPos] = 'X';
-                    notValid = false;
-                } else{
-                    System.out.println("That spot is already taken");
-                }
-            
-        } while (notValid);
-
+        System.out.println("Would you like to play against a player or computer? Type \"Player\" or \"Computer\"");
+        computerOrPlayer = input.nextLine().toLowerCase();
+        if(computerOrPlayer.equals("player")) {
+            return true;
+        } else if(computerOrPlayer.equals("computer")) {
+            return false;
+        } else {
+            System.out.println("Invalid Response! Please try again");
+            continue;
+        } } while(true/*!computerOrPlayer.equals("player") || !computerOrPlayer.equals("computer")*/);
+        //return false;
     }
 
-    public void playO(){
+    public void computerTurn() {
         boolean notValid = true;
         int xPos;
         int yPos;
 
         do {
-            System.out.println("What Row? 1-3");
-            xPos = clamp(input.nextInt(),1,3) - 1;
-            System.out.println("What Column? 1-3");
-            yPos = clamp(input.nextInt(),1,3) - 1;
+            xPos = (int) Math.random() * 2; 
+            yPos = clamp(input.nextInt(), 1, 3) - 1;
 
-            if(board[xPos][yPos] == '_'){
-                    board[xPos][yPos] = 'O';
-                    notValid = false;
-                } else{
-                    System.out.println("That spot is already taken");
-                }
+            if (board[xPos][yPos] == '_') {
+                board[xPos][yPos] = 'O';
+                notValid = false;
+            }
         } while (notValid);
     }
 
-    public String checkWinner(){
-        for(int i = 0; i < 8; i++){
+    public void playX() {
+        boolean notValid = true;
+        int xPos;
+        int yPos;
+        do {
+            System.out.println("What Row? 1-3");
+            xPos = clamp(input.nextInt(), 1, 3) - 1;
+            System.out.println("What Column? 1-3");
+            yPos = clamp(input.nextInt(), 1, 3) - 1;
+
+            if (board[xPos][yPos] == '_') {
+                board[xPos][yPos] = 'X';
+                notValid = false;
+            } else {
+                System.out.println("That spot is already taken");
+            }
+
+        } while (notValid);
+
+    }
+
+    public void playO() {
+        boolean notValid = true;
+        int xPos;
+        int yPos;
+
+        do {
+            System.out.println("What Row? 1-3");
+            xPos = clamp(input.nextInt(), 1, 3) - 1;
+            System.out.println("What Column? 1-3");
+            yPos = clamp(input.nextInt(), 1, 3) - 1;
+
+            if (board[xPos][yPos] == '_') {
+                board[xPos][yPos] = 'O';
+                notValid = false;
+            } else {
+                System.out.println("That spot is already taken");
+            }
+        } while (notValid);
+    }
+
+    public String checkWinner() {
+        for (int i = 0; i < 8; i++) {
             line = null;
             switch (i) {
                 case 0:
@@ -115,18 +145,17 @@ public class TicTacToeBoard {
                     break;
             }
 
-            //System.out.println("Line: " + line);
+            // System.out.println("Line: " + line);
             if (line.equals("XXX")) {
-            return "Player X won";
-            }
-            else if (line.equals("OOO")){
+                return "Player X won";
+            } else if (line.equals("OOO")) {
                 return "Player O won";
             }
         }
         return null;
     }
 
-    private int clamp(int value, int min, int max){
+    private int clamp(int value, int min, int max) {
         return Math.max(min, Math.min(max, value));
     }
 }
